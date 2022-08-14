585389013ad5d43b7d290dec398c93473ad5b1b6
A90ur5 <38530574+A90ur5@users.noreply.github.com>
2022-08-15 01:12:46 +0800

    First Bad Version

---
+++ python/easy/First Bad Version.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l, r = 0, n
+        res = 0
+        while l <= r:
+            m = (l + r) // 2
+            if isBadVersion(m):
+                res = m
+                r = m - 1
+            else:
+                l = m + 1
+        return res
