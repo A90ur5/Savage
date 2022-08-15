cfc99a0b8e70c078253ff1722f9141bc8d8702f7
A90ur5 <38530574+A90ur5@users.noreply.github.com>
2022-08-16 01:03:44 +0800

    Merge Sorted Array

---
+++ python/easy/Merge Sorted Array.py
@@ -0,0 +1,18 @@
+class Solution:
+    #def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1, m, nums2, n):
+        i ,j = m-1, n-1
+        for k in range(m+n-1, -1, -1):
+            if i < 0:
+                nums1[:j+1] = nums2[:j+1]
+
+            elif j < 0:
+                break
+
+            else:
+                if nums1[i] >= nums2[j]:
+                    nums1[k] = nums1[i]
+                    i -= 1
+                else:
+                    nums1[k] = nums2[j]
+                    j -= 1
